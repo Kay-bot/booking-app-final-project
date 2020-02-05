41beb3998a3d019d5715dd7e2df95b21da04ad32
KAY S <KS.KWANRUAN@GMAIL.com>
2020-02-06 03:59:23 +1100

    edit syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
 
     skip_before_action :verify_authenticity_token
-    before_action :authenticate_request, except: [:new, :create, :index, :authenticate] :raise => false
-        
+    before_action :authenticate_request, except: [:new, :create, :index, :authenticate], :raise => false
+    # before_action :route_user
+
     attr_reader :current_user
    
    
@@ -12,6 +13,16 @@ class ApplicationController < ActionController::Base
 
 
     private
+
+    # def route_user
+    #   target_path = '/authenticate'      
+    #   if user_signed_in?       
+    #     target_path =  '/admin/admins/dashboard' if current_user.admin?       
+    #     target_path =  '/parent/parents/dashboard' if current_user.parent?       
+    #     target_path =  '/mosque/mosques/dashboard' if current_user.mosque?
+    #   end    
+    #   redirect_to target_path
+    # end
 
     def authenticate_request
       @current_user = AuthorizeApiRequest.call(request.headers).result
