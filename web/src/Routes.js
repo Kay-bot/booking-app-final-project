daffe36c6ab3a27b06b44a29c8af3f7554807d51
KAY S <KS.KWANRUAN@GMAIL.com>
2020-02-06 01:32:55 +1100

    Convert class component to Functional Component with Hooks

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import "./styles/App.css";
 import HomeHero from "./styles/HomeHero";
 
@@ -10,7 +10,7 @@ import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import SingleLesson from "./pages/SingleLesson";
-import Error from "./pages/Error";
+// import Error from "./pages/Error";
 
 import Navbar from "./navbar/Navbar";
 
@@ -18,51 +18,41 @@ import Schedules from "./components/Schedules";
 import LessonForm from "./forms/LessonForm";
 import { AuthContext } from "./context/auth";
 
-class Routes extends Component {
-  constructor(props) {
-    super(props);
-    let auth = JSON.parse(sessionStorage.getItem("auth"));
-    this.state = {
-      navbarOpen: false,
-      user: {
-        isLoggedIn: !!auth ? true : false,
-        currentUser: null,
-        currentUserId: null,
-        loginErrorMessage: ""
-      }
-    };
-  }
-  handleNavbar = () => {
-    this.setState({ navbarOpen: !this.state.navbarOpen });
+function Routes(props) {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [authTokens, setAuthTokens] = useState();
+
+  const handleNavbar = () => {
+    setNavbarOpen(!navbarOpen);
   };
 
-  render() {
-    return (
-      <AuthContext.Provider value={{ ...this.state.user }}>
-        <BrowserRouter>
-          <Navbar
-            navbarState={this.state.navbarOpen}
-            handleNavbar={this.handleNavbar}
-          />
-          <Switch>
-            <Route exact path="/">
-              <HomeHero>
-                <h2>Welcome to the class room!</h2>
-              </HomeHero>
-            </Route>
-            <Route exact path="/lessons" component={Lessons} />
-            <Route exact path="/lessons/:id" component={SingleLesson} />
-            <Route exact path="/schedules" component={Schedules} />
-            <Route exact path="/add-lessons" component={LessonForm} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/Register" component={Register} />
-            <Route component={Error} />
-            <PrivateRoute exact path="/admin" component={Admin} />
-          </Switch>
-        </BrowserRouter>
-      </AuthContext.Provider>
-    );
-  }
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  };
+
+  return (
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <BrowserRouter>
+        <Navbar navbarState={navbarOpen} handleNavbar={handleNavbar} />
+        <Switch>
+          <Route exact path="/">
+            <HomeHero>
+              <h2>Welcome to the class room!</h2>
+            </HomeHero>
+          </Route>
+          <Route exact path="/lessons" component={Lessons} />
+          <Route exact path="/lessons/:id" component={SingleLesson} />
+          <Route exact path="/schedules" component={Schedules} />
+          <Route exact path="/add-lessons" component={LessonForm} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/Register" component={Register} />
+          {/* <Route component={Error} /> */}
+          <PrivateRoute exact path="/admin" component={Admin} />
+        </Switch>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
 }
 
 export default Routes;
