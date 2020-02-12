bd888139ff9470ada75f7f0e8a1941ac85e6438d
KAY S <KS.KWANRUAN@GMAIL.com>
2020-02-12 12:34:07 +1100

    manage-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   resources :lesson_payments
   resources :bookings
-  resources :schedules
   resources :clients
   resources :trainers
-  resources :lessons
-  # resources :users
   resources :accounts
+  resources :lessons
 
+ #schedules
+  get ' /schedules/:id', to: 'schedules#show'
+  post '/schedules', to: 'schedules#create'
+
+  #users
   post 'login', to: 'users#login'
   get '/users', to: 'users#index'
   post  '/users', to: 'users#create'
@@ -18,8 +21,7 @@ Rails.application.routes.draw do
   post 'lesson/payment', to: 'users#create_client'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '*path', to: "homepage#fallback_index_html", constraints: ->(request) do
-    puts "Falling back to index.html..."
-    !request.xhr? && request.format.html?
-  end
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  !request.xhr? && request.format.html?
+end
 end
